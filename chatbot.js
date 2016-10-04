7b588ebdf723ed96702d5ff988a9806178b1c5bc
Aleandro D <alejandrodelatorre05.25@gmail.com>
2016-10-04 10:40:30 -0700

    chat
    box

--- chatbot.js
+++ chatbot.js
@@ -1,13 +1,23 @@
-function askQuestion() {
-  var input = document.getElementById("input").value;
-  var giveresponse = responses[input]
-  document.getElementById("chat-area").textContent += input + ": " + giveresponse + " ";
-}
 var responses = {
   "how are you" : "I am good how are you",
-  "What is your name" : "My name is Little light",
-  "How old are you" : "I am 1754 year's old",
-  "What time is it" : "I think its time for you to get a watch",
-  "Hello" : "Hi there",
+  "what is your name" : "My name is Little light",
+  "how old are you" : "I am 1754 year's old",
+  "what time is it" : "I think its time for you to get a watch",
+  "hello" : "Hi there",
 
+}
+
+function askQuestion() {
+  var input = document.getElementById("input").value;
+  input = input.toLowerCase();
+  var giveresponse = responses[input]
+  if (giveresponse == undefined){
+    var fallBackResponse = ["Sorry i didnt quite get that.", ""]
+    var rand = Math.floor((Math.random() * fallBackResponse.length));
+    var fallBack = fallBackResponse[rand]
+    document.getElementById("chat-area").textContent += input + ": " + fallBack + " ";
+  }
+  else {
+      document.getElementById("chat-area").textContent += input + ": " + giveresponse + " ";
+  }
 }
